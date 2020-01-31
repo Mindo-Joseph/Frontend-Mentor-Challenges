a16d5348651156b3694e0574303fedd38866d625
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-01-31 03:14:13 +0300

    fix error

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -11,5 +11,23 @@ document.getElementById("registration-form").onsubmit = function() {
         document.getElementById("fname_error").innerHTML = fname_error;
         submit = false;
     }
+    if (y == null || y == "") {
+        lname_error = "Last Name cannot be empty";
+        document.getElementById("lname_error").innerHTML = lname_error;
+        submit = false;
+    }
+    if (z == null || z == "") {
+        email_error = "Email Name cannot be empty";
+        document.getElementById("email_error").innerHTML = email_error;
+        submit = false;
+    }
+    return submit;
 
 }
+function removeWarning() {
+    document.getElementById(this.id + "_error").innerHTML = "";
+}
+
+document.getElementById("fname").onkeyup = removeWarning;
+document.getElementById("lname").onkeyup = removeWarning;
+document.getElementById("email").onkeyup = removeWarning;
